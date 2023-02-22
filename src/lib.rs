49c6f936f6f46c21cebd7de85c86b33601d4d53d
Antoine Marneur <antoinemarneur@antoines-macbook-air.home>
2023-02-22 21:50:03 +0100

    add new structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,11 @@
-pub mod messages;
-pub mod likes;
+pub mod config;
+pub mod router;
+pub mod message;
+pub mod like;
+pub mod error;
+
+pub use error::{Error, ResultExt};
+
+pub type Result<T, E = Error> = std::result::Result<T, E>;
+
+use tower_http::trace::TraceLayer;
