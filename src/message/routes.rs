3e26ca4feff9559e8fe495b45bee0a162dedf95d
Antoine Marneur <antoinemarneur@antoines-macbook-air.home>
2023-03-06 22:33:36 +0100

    add post for /message/:id

--- src/message/routes.rs
+++ src/message/routes.rs
@@ -8,10 +8,13 @@ pub fn router() -> Router {
     Router::new()
                 .route(
                     "/messages",
-                    get(messages::list_messages).post(messages::create_message),
+                    get(messages::get_messages)
+                    .post(messages::create_message),
                 )
                 .route(
                     "/message/:id",
-                    get(messages::list_message).delete(messages::delete_message),
+                    get(messages::get_message)
+                    .delete(messages::delete_message)
+                    .post(messages::create_comment),
                 )
 }
