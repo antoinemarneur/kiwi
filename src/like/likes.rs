eedd7da067f96fad7711e0e1323238dfb54e5ce1
Antoine Marneur <antoinemarneur@antoines-macbook-air.home>
2023-03-21 23:12:26 +0100

    add macros

--- src/like/likes.rs
+++ src/like/likes.rs
@@ -1,5 +1,10 @@
-use crate::{Result};
-use crate::router::server::ApiContext;
+use crate::{
+    Result,
+    router::{
+        server::ApiContext,
+        extractor::AuthUser,
+    }
+};
 use uuid::Uuid;
 use time::PrimitiveDateTime;
 use serde::{Serialize};
@@ -16,39 +21,52 @@ pub struct Like {
 }
 
 pub async fn create_like(
+    auth_user: AuthUser,
     ctx: Extension<ApiContext>,
     Path(id): Path<Uuid>
 ) -> Result<Json<Like>> {
-    let like = sqlx::query_as::<_, Like>(
+    let like = sqlx::query!(
         r#"
-            insert into like 
+            insert into like (id, message_id)
             values ($1, $2)
             returning
-                id,
-                message,
-                created_at
-        "#
+                id as "id!: Uuid",
+                message_id as "message_id!: Uuid",
+                created_at as "created_at!: PrimitiveDateTime"
+        "#,
+        auth_user.user_id,
+        id
     )
-    .bind(Uuid::new_v4())
-    .bind(id)
     .fetch_one(&ctx.db)
     .await
     .unwrap();
 
-    Ok(Json(like))
+    Ok(Json(
+        Like {
+            id: like.id,
+            message_id: like.message_id,
+            created_at: like.created_at
+        }
+    ))
 }
 
 pub async fn get_likes(
+    _: AuthUser,
     ctx: Extension<ApiContext>,
     Path(id): Path<String>
 ) -> Result<Json<Vec<Like>>> {
-    let likes = sqlx::query_as::<_, Like>(
+    let likes = sqlx::query_as!(
+        Like,
         r#"
-            select * from like 
+            select
+                id as "id!: Uuid",
+                message_id as "message_id!: Uuid",
+                created_at as "created_at!: PrimitiveDateTime"
+            from like 
             where like.message_id = $1
-        "#
+        "#,
+        id
     )
-    .bind(id)
     .fetch_all(&ctx.db)
     .await
     .unwrap();
