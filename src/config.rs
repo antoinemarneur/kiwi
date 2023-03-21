e1d31e6008b573920c97bd5f9f0ca7dcdd9a3f37
Antoine Marneur <antoinemarneur@antoines-macbook-air.home>
2023-03-21 23:08:37 +0100

    add hmac

--- src/config.rs
+++ src/config.rs
@@ -5,4 +5,7 @@ pub struct Config {
     // The connection URL for the database this application should use.
     #[clap(long, env)]
     pub database_url: String,
+
+    #[clap(long, env)]
+    pub hmac_key: String,
 }
