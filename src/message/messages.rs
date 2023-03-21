c0aa9d2d5ac907be15973b94006b702987ea058c
Antoine Marneur <antoinemarneur@antoines-macbook-air.home>
2023-03-21 23:11:36 +0100

    add sqlx macros

--- src/message/messages.rs
+++ src/message/messages.rs
@@ -1,5 +1,12 @@
-use crate::{Result, Error, ResultExt};
-use crate::router::server::ApiContext;
+use crate::{
+    Result,
+    Error,
+    ResultExt,
+    router::{
+        server::ApiContext,
+        extractor::AuthUser,
+    }
+};
 use uuid::Uuid;
 use time::PrimitiveDateTime;
 use serde::{Deserialize, Serialize};
@@ -11,7 +18,7 @@ use axum::{
 #[derive(Debug, Serialize, sqlx::FromRow)]
 pub struct Message {
     id: Uuid,
-    pub author_id: Uuid,
+    author_id: Uuid,
     created_at: PrimitiveDateTime,
     message: String,
     message_parent_id: Option<Uuid>,
@@ -25,9 +32,16 @@ pub struct MessageRequest {
 pub async fn get_messages(
     ctx: Extension<ApiContext>
 ) -> Result<Json<Vec<Message>>> {
-    let messages = sqlx::query_as::<_, Message>(
+    let messages = sqlx::query_as!(
+        Message,
         r#"
-            select * from message
+            select 
+                id as "id!: Uuid",
+                author_id as "author_id!: Uuid",
+                created_at as "created_at!: PrimitiveDateTime",
+                message,
+                message_parent_id as "message_parent_id!: Option<Uuid>"
+            from message
         "#
     )
     .fetch_all(&ctx.db)
@@ -37,68 +51,90 @@ pub async fn get_messages(
 }
 
 pub async fn create_message(
+    auth_user: AuthUser,
     ctx: Extension<ApiContext>,
     Json(input): Json<MessageRequest>
 ) -> Result<Json<Message>> {
-    let message = sqlx::query_as::<_, Message>(
+    let message_id = Uuid::new_v4();
+
+    let message = sqlx::query!(
         r#"
             insert into message (id, author_id, message)
             values ($1, $2, $3)
             returning
-                id,
-                author_id,
-                created_at,
-                message,
-                message_parent_id
-        "#
+                id as "id!: Uuid",
+                author_id as "author_id!: Uuid",
+                created_at as "created_at!: PrimitiveDateTime",
+                message as "message!",
+                message_parent_id as "message_parent_id!: Option<Uuid>"
+        "#,
+        message_id,
+        auth_user.user_id,
+        input.message
     )
-    .bind(Uuid::new_v4())
-    .bind(Uuid::new_v4())
-    .bind(input.message)
     .fetch_one(&ctx.db)
     .await
     .on_constraint("message", |_| {
         Error::unprocessable_entity([("message", format!("duplicate message id"))])
     })?;
     
-    Ok(Json(message))
+    Ok(Json(
+        Message { 
+            id: message.id,
+            author_id: message.author_id,
+            created_at: message.created_at,
+            message: message.message,
+            message_parent_id: message.message_parent_id 
+        }
+    ))
 }
 
 pub async fn get_message(
+    _: AuthUser,
     ctx: Extension<ApiContext>,
     Path(id): Path<Uuid>
 ) -> Result<Json<Message>> {
-    let message = sqlx::query_as::<_, Message>(
+    let message = sqlx::query!(
         r#"
             select 
-                id,
-                author_id,
-                created_at,
-                message,
-                message_parent_id
+                id as "id!: Uuid",
+                author_id as "author_id!: Uuid",
+                created_at as "created_at!: PrimitiveDateTime",
+                message as "message!",
+                message_parent_id as "message_parent_id!: Option<Uuid>"
             from message
             where message.id = $1
-        "#
+        "#,
+        id
     )
-    .bind(id)
     .fetch_optional(&ctx.db)
     .await?
     .ok_or(Error::NotFound)?;
 
-    Ok(Json(message))
+    Ok(Json(
+        Message { 
+            id: message.id,
+            author_id: message.author_id,
+            created_at: message.created_at,
+            message: message.message,
+            message_parent_id: message.message_parent_id 
+        }
+    ))
 }
 
 pub async fn delete_message(
+    auth_user: AuthUser,
     ctx: Extension<ApiContext>,
     Path(id): Path<Uuid>
 ) -> Result<()> {
-    sqlx::query(
+    sqlx::query!(
         r#"
             delete from message
-            where message.id = $1
-        "#
+            where message.id = $1 and author_id = $2
+        "#,
+        id,
+        auth_user.user_id
     )
-    .bind(id)
     .fetch_optional(&ctx.db)
     .await?;
 
@@ -106,31 +142,42 @@ pub async fn delete_message(
 }
 
 pub async fn create_comment(
+    auth_user: AuthUser,
     ctx: Extension<ApiContext>,
     Path(id): Path<Uuid>,
     Json(input): Json<MessageRequest>
 ) -> Result<Json<Message>> {
-    let message = sqlx::query_as::<_, Message>(
+    let message_id = Uuid::new_v4();
+
+    let message = sqlx::query!(
         r#"
             insert into message (id, author_id, message, message_parent_id)
             values ($1, $2, $3, $4)
             returning
-                id, 
-                author_id,
-                created_at,
-                message,
-                message_parent_id
-        "#
+                id as "id!: Uuid",
+                author_id as "author_id!: Uuid",
+                created_at as "created_at!: PrimitiveDateTime",
+                message as "message!",
+                message_parent_id as "message_parent_id!: Option<Uuid>"
+        "#,
+        message_id,
+        auth_user.user_id,
+        input.message,
+        id
     )
-    .bind(Uuid::new_v4())
-    .bind(Uuid::new_v4())
-    .bind(input.message)
-    .bind(id)
     .fetch_one(&ctx.db)
     .await
     .on_constraint("message", |_| {
         Error::unprocessable_entity([("message", format!("duplicate message id"))])
     })?;
     
-    Ok(Json(message))
+    Ok(Json(
+        Message { 
+            id: message.id,
+            author_id: message.author_id,
+            created_at: message.created_at,
+            message: message.message,
+            message_parent_id: message.message_parent_id 
+        }
+    ))
 }
