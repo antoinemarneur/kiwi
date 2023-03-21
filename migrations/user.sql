de0967158e6493ce35b599f01954ae2ea5a3d325
Antoine Marneur <antoinemarneur@antoines-macbook-air.home>
2023-03-21 23:12:48 +0100

    change user schema

--- migrations/user.sql
+++ migrations/user.sql
@@ -1,10 +1,10 @@
 create table user (
-    id            uuid primary key,
-    username      text                  unique not null,
-    email         text                  unique not null,
-    bio           text                  not null default '',
-    image         text,
-    password_hash text                  not null,
-    created_at    timestamp             not null default current_timestamp,
-    updated_at    timestamp
+    id              uuid primary key not null,
+    username        text                  unique not null,
+    email           text                  unique not null,
+    bio             text                  not null default '',
+    image           text,
+    password_hash   text                  not null,
+    created_at      timestamp             not null default current_timestamp,
+    updated_at      timestamp
 );
